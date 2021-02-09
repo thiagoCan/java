90a4679b822007b654e59e9768dad2ba80859e05
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-02-09 10:09:13 -0300

    Add files via upload

---
+++ FileBuffer/versão1/Program.java
@@ -0,0 +1,42 @@
+package Exer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\Thiago Fernandes\\OneDrive - Fatec Centro Paula Souza\\Documents\\POO\\udemy\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null) {
+					fr.close();
+				}
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
