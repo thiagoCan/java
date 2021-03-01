6848592b2a6bb1b7b41ce260279f41524f0fdef8
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-03-01 11:01:27 -0300

    Add files via upload

---
+++ Interface/default method/aulas/InterestService.java
@@ -0,0 +1,16 @@
+package aulas;
+
+import java.security.InvalidParameterException;
+
+public interface InterestService {
+	
+	double getInterestRate();
+	
+	default double payment (double amount, int months) {
+		if (months < 1) {
+			throw new InvalidParameterException("Months must be greater than zero.");
+		}
+		
+		return amount * Math.pow(1 + getInterestRate()/100, months);
+	}
+}
---
+++ Interface/default method/aulas/Program.java
@@ -0,0 +1,29 @@
+package aulas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		//upcasting
+		InterestService is = new BrazilInterestService(2);
+		double payment = is.payment(amount, months);
+		
+		System.out.println("Payment after " + months + " months:");
+		System.out.println((String.format("%.2f", payment)));
+		
+		sc.close();
+	}
+
+}
