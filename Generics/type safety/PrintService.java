ca38b8717c842739b5fd3e4b5e14582df3c17f47
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-03-01 20:08:49 -0300

    Add files via upload

---
+++ Generics/type safety/PrintService.java
@@ -0,0 +1,32 @@
+package aulas;
+
+import java.awt.IllegalComponentStateException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<G> {
+
+	private List<G> list = new ArrayList<>();
+	
+	public void addValue(G value) {
+		list.add(value);
+	}
+	
+	public G first() {
+		if(list.isEmpty()) {
+			throw new IllegalComponentStateException("List is empty");
+		}
+		return list.get(0);
+	}
+	
+	public void print() {
+		System.out.print("[");
+		if (!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.println("]");
+	}
+}
